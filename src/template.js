1213e3c1c99f7ffaf23daffec7f40fe6cadbe586
hicey <hiceycook@gmail.com>
2021-08-26 14:37:03 -0400

    fix

--- src/template.js
+++ src/template.js
@@ -1,46 +1,75 @@
-const generateReadMe = (res) => {
+const generateReadMe = res => {
     return `
-    # ${res.projectName}
+    # ${res.projectName.toUpperCase()}
+
     ## Table of Contents
-    - [Description](#Description)
-    - [Installation](#Installation)
-    - [Usage](#Usage)
-    - [License](#License)
-    - [Contributing](#Contributing)
-    - [Tests](#Tests)
-    - [Questions](#Questions)
-    <div id='Desciption'/>
-    ## Description
-    ${res.description}
-    <div id='Installation'/>
-    ## Installation
-    ${res.instructions}
-    <div id='Usage'/>
-    ## Usage
-    ${res.usage}
-    <div id='License'/>
-    ## License       
-    ${res.license}  
-    ${renderBadge(res.license)}
-    <div id='Contributing'/>
-    ## Contributing
-    ${res.contribution} 
-    <div id='Tests'/>
-    ## Tests
-    ${res.testing}
-    <div id='Questions'/>
-    ## Questions
-    For additional questions or concerns regarding this project, please contact the information below:
-    **Contact information**  
-    GitHub: github.com/${res.githubUser}  
-    Email: ${res.email}
-    `;
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+<div id='Desciption'/>
+## Description
+${res.description}
+<div id='Installation'/>
+## Installation
+${res.installation}
+<div id='Usage'/>
+## Usage
+${res.usage}
+<div id='License'/>
+## License       
+${res.license}  
+${renderBadge(res.license)}
+<div id='Contributing'/>
+## Contributing
+${res.contribution} 
+<div id='Tests'/>
+## Tests
+${res.test}
+<div id='Questions'/>
+## Questions
+For additional questions or concerns regarding this project, please contact the information below:
+**Contact information**  
+GitHub: github.com/${res.githubUser}  
+Email: ${res.email}
+`;
 };
 
+
 function renderBadge(license) {
-    if (license !== 'UNLICENSED') {
-    } return `![license](https://img.shields.io/badge/license-${license}-green.svg)`
+    if (license) {
+        return `![license](https://img.shields.io/badge/license-Apache%202.0-blue)`;
+    };
 }
 
 // to allow 'generateReadMe' to be called from index.js
 module.exports = generateReadMe;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
